1efda62bde5b9c7e99a8b137544ea53becb54e85
Artur Staszczyk <arturs@qualtrics.com>
2018-10-14 13:29:00 +0200

    Include python

--- EverDo.pro
+++ EverDo.pro
@@ -7,6 +7,9 @@ SOURCES += main.cpp
 
 RESOURCES += qml.qrc
 
+INCLUDEPATH += /usr/local/Cellar/python@2/2.7.14_3/Frameworks/Python.framework/Versions/2.7/include/python2.7 /usr/local/Cellar/python@2/2.7.14_3/Frameworks/Python.framework/Versions/2.7/include/python2.7
+LIBS += -L/usr/local/opt/python@2/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config -lpython2.7 -ldl -framework CoreFoundation
+
 # Additional import path used to resolve QML modules in Qt Creator's code model
 QML_IMPORT_PATH = $$PWD/qml
 
--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include <QFAppDispatcher>
 #include <QuickFlux>
 
+#include <Python.h>
+
 int main(int argc, char *argv[])
 {
     qputenv("QML_DISABLE_DISK_CACHE", "true");
 
-    char* a[0];
-    int b = 0;
-    QGuiApplication app(b, a);
+    QGuiApplication app(argc, argv);
 
     registerQuickFluxQmlTypes(); // It is not necessary to call this function if the QuickFlux library is installed via qpm
 
@@ -18,6 +18,12 @@ int main(int argc, char *argv[])
 
     QFAppDispatcher* dispatcher = QFAppDispatcher::instance(&engine);
     dispatcher->dispatch("startApp");
+
+    Py_SetProgramName(argv[0]);  /* optional but recommended */
+    Py_Initialize();
+    PyRun_SimpleString("from time import time,ctime\n"
+                         "print 'Today is',ctime(time())\n");
+    Py_Finalize();
 
     return app.exec();
 }
